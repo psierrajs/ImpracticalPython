f1523becb5da75cd3dd61a50bd64631e90b36c32
psierrajs <psierra.js@gmail.com>
2025-08-10 13:46:57 +0200

    Change logic to simplify code

--- ImpracticalPython/Chapter01/pig_latin_praxctice.py
+++ ImpracticalPython/Chapter01/pig_latin_praxctice.py
@@ -5,17 +5,18 @@ def main():
     while True:
         introduced_word = input("Please insert word to translate into pig latin: \n")
 
-        if introduced_word[0].isalpha():
-
-            if introduced_word[0] in vowels_to_check:
-                pig_latin_word = introduced_word + "way"
-            else:
-                pig_latin_word = introduced_word[1:] + introduced_word[0] + "ay"
+        if introduced_word.isalpha() and introduced_word[0] in vowels_to_check :
+            pig_latin_word = introduced_word + "way"
             print(f"Pig Lating translation of '{introduced_word}' is '{pig_latin_word}'")
-            try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
-            if try_again.lower() == "n":
-                print("Bye bye!")
-                break
+        elif introduced_word.isalpha():
+            pig_latin_word = introduced_word[1:] + introduced_word[0] + "ay"
+            print(f"Pig Lating translation of '{introduced_word}' is '{pig_latin_word}'")
+        else:
+            print("Sorry, word need to be in alphanumeric characters!")
+        try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
+        if try_again.lower() == "n":
+            print("Bye bye!")
+            break
 
 if __name__ == "__main__":
     main()
