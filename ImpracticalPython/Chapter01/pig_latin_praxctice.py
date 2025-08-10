f002d9b272434cd46bd5d7434cc0fbf272a09870
psierrajs <psierra.js@gmail.com>
2025-08-10 13:52:46 +0200

    Refactor adding print function

--- ImpracticalPython/Chapter01/pig_latin_praxctice.py
+++ ImpracticalPython/Chapter01/pig_latin_praxctice.py
@@ -7,16 +7,20 @@ def main():
 
         if introduced_word.isalpha() and introduced_word[0] in vowels_to_check :
             pig_latin_word = introduced_word + "way"
-            print(f"Pig Lating translation of '{introduced_word}' is '{pig_latin_word}'")
+            print_pig_latin_word(introduced_word, pig_latin_word)
         elif introduced_word.isalpha():
             pig_latin_word = introduced_word[1:] + introduced_word[0] + "ay"
-            print(f"Pig Lating translation of '{introduced_word}' is '{pig_latin_word}'")
+            print_pig_latin_word(introduced_word, pig_latin_word)
         else:
             print("Sorry, word need to be in alphanumeric characters!")
         try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
         if try_again.lower() == "n":
             print("Bye bye!")
             break
+
+def print_pig_latin_word(original_word, translated_word):
+    """This is the function that prints the message with the word in pig latin"""
+    print(f"Pig Lating translation of '{original_word}' is '{translated_word}'")
 
 if __name__ == "__main__":
     main()
