1572a8ac7ca00df85f177af0621ff2f0328d3981
psierrajs <psierra.js@gmail.com>
2025-08-10 12:11:09 +0200

    Testing

--- ImpracticalPython/Chapter01/pseudonimous.py
+++ ImpracticalPython/Chapter01/pseudonimous.py
@@ -28,7 +28,7 @@ while True:
     print("{} {}".format(firstName, lastName), file=sys.stderr)
     print("\n\n")
 
-    try_again = input("\n\Try again? (Press Enter else n to quit)\n ")
+    try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
     if try_again.lower() == "n":
         break
-input("\nPress Enter to exit.")
+input("\nPress Enter to exit.\n")
